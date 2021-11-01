18e5073d371e71b32cbf588b994eef658cfd086c
ChaehwanLee <oasisinyourmind@gmail.com>
2021-11-01 23:54:04 +0900

    first commit

---
+++ a.py
@@ -0,0 +1,16 @@
+print("Python Programming 공부")
+import keyword
+print(keyword.kwlist)
+
+print("# print 1")
+print("Hello Python Programming")
+print()
+
+print("# print multiple")
+print("1, 2, 3, 4, 5, 6, 7")
+print("Good day but it's too far away")
+print()
+
+print("# Nothing on it")
+print("--- line ---")
+print()
